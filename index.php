be718b26aa137731df7510b32eaa382e625cabf5
MlrariA <kuzmin1914@gmail.com>
2023-09-18 22:46:50 +0700

    first

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <html lang='ru'>
 <head>
+    <title>homework</title>
 </head>
 <body>
-    <p>hello world</p>
-    <?php
-//    var_dump($_POST);
-    if  (isset($_POST["login"])) {
-        foreach ($_POST["login"] as $key => $value){
-            echo sprintf("<p>%s: %s</p>", $key, $value);
-        }
-//        echo '<p>'. $_POST["login"]["username"].'</p>';
-    } else {
-        echo ' <form action="index.php" name="login" method="post">
-      <label>
-          username:
-          <input type="text" name="login[username]" >
-      </label>
+<p>hello world</p>
+<?php
+if (isset($_POST["login"])) {
+    foreach ($_POST["login"] as $key => $value) {
+        echo sprintf("<p>%s: %s</p>", $key, $value);
+    }
+} else {
+    echo '
+    <form action="index.php" name="login" method="post">
+        <label>
+            username:
+            <input type="text" name="login[username]" >
+        </label>
         <label>
             password:
             <input type="password" name="login[password]">
         </label>
-        <button type="submit"  >confirm </button>
-    </form>' ;}
-    ?>
+        <button type="submit">confirm</button>
+    </form>
+';
+}
+?>
 </body>
 </html>
 
